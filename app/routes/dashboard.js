7ecd4a03508f31355ac8c08aa5f5e99e93df6e7f
Gustavo Clavijo <gustavoc@nodus.com.uy>
2018-11-30 14:59:05 -0300

    Todo OK

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const { issueStore } = require('../models');
+
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('dashboard', {});
+  const openIssues = issueStore.getAllOpen();
+  const openIssuesCount = openIssues.length;
+  const highIssuesCount = calculatePercentage(openIssues, "High");
+  const mediumIssuesCount = calculatePercentage(openIssues, "Medium");
+
+  res.render('dashboard', {
+    openIssuesCount,
+    highIssuesCount,
+    mediumIssuesCount
+  });
 });
+
+function calculatePercentage(issues, severity) {
+  let percentage = 0;
+
+  if (issues.length > 0) {
+    percentage =
+      issues.filter(issue => issue.severity === severity).length / issues.length;
+  }
+
+  return percentage;
+}
 
 module.exports = router;
--- test/integration/dashboard_spec.js
+++ test/integration/dashboard_spec.js
@@ -1,19 +1,72 @@
 describe('Dashboard', function() {
-    it('shows the number of open issues', function(){
+    it('shows the number of open issues', function() {
         cy.fixture({
             title: "Issue 1",
-            status:"new"
-            
+            status: "open"
         });
         cy.fixture({
             title: "Issue 2",
-            status:"open"
+            status: "open"
         });
         cy.fixture({
             title: "Issue 3",
-            status:"close"
+            status: "closed"
         });
+
         cy.visit('/dashboard');
+
         cy.contains('2').should('exist');
+    });
+
+    it('show high severity gauge', function() {
+        cy.fixture({
+            title: "Issue 1",
+            status: "open",
+            severity: "High"
+        });
+        cy.fixture({
+            title: "Issue 2",
+            status: "open",
+            severity: "Low"
+        });
+
+        cy.visit('/dashboard');
+
+        cy
+            .get("[data-test-high-gauge]")
+            .should('exist')
+            .should("contain", "High")
+            .should("contain", "50%");
+    });
+
+    it('show 0% when no open issues', function() {
+        cy.visit('/dashboard');
+
+        cy
+            .get("[data-test-high-gauge")
+            .should("exist")
+            .should("contain", "High")
+            .should("contain", "0%");
+    });
+
+    it('show medium severity gauge', function() {
+        cy.fixture({
+            title: "Issue 1",
+            status: "open",
+            severity: "Medium"
+        });
+        cy.fixture({
+            title: "Issue 2",
+            status: "open",
+            severity: "Low"
+        });
+
+        cy.visit('/dashboard');
+
+        cy
+            .get("[data-test-medium-gauge]")
+            .should('exist')
+            .should("contain", "Medium")
+            .should("contain", "50%");
     });
 });
