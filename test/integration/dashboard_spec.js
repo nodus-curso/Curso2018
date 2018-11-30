809ed44710c92bb59ce9f54e00736a3ca83f376a
Gustavo Clavijo <gustavoc@nodus.com.uy>
2018-11-30 11:21:16 -0300

    Nuevo commit 30/11

--- test/integration/dashboard_spec.js
+++ test/integration/dashboard_spec.js
@@ -1,2 +1,18 @@
 describe('Dashboard', function() {
+    it('shows the number of open issues', function(){
+        cy.fixture({
+            title: "Issue 1",
+            status:"new"
+        });
+        cy.fixture({
+            title: "Issue 2",
+            status:"open"
+        });
+        cy.fixture({
+            title: "Issue 3",
+            status:"close"
+        });
+        cy.visit('/dashboad');
+        cy.contains('2').should('exist');
+    });
 });
